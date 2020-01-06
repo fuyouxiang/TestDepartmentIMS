9df1c28bbd9a4e90cd3243ff5b44641d3726f5bb
Administrator <Administrator@10.6.237.71>
2020-01-06 13:40:14 +0800

    数据库增加开关，控制所有ping方法是否启用

--- src/cn/com/shxt/utils/Ping.java
+++ src/cn/com/shxt/utils/Ping.java
@@ -6,8 +6,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import cn.com.shxt.model.PageBean;
 
 
 
@@ -16,32 +19,47 @@ import java.util.regex.Pattern;
 public class Ping {
 
 		
-	public synchronized  boolean ping(String urlStr) {
+	public synchronized  boolean ping(String urlStr) throws SQLException {
 		
-		int counts = 0;
-		while (counts < 1) {
-		try {
-			URL url = new URL(urlStr);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    		con.setUseCaches(false);
-    		con.setReadTimeout(1500);
-    		con.setConnectTimeout(1500);
-			int state = con.getResponseCode();
-			System.out.println(counts +"= "+state);
-			if (state == 200) {
-				System.out.println("URL可用！");
-				return true;
-			}
-			break;
-		}catch (Exception ex) {
-			counts++;
-			System.out.println("URL不可用，连接第"+counts+"次");
-			urlStr = null;
-			continue;
-			}
-		}
+		
+		String ConfigName = "isPing";
+		String sql ="select * from sys_config where name ='"+ConfigName+"'";
+		System.out.println("查询配置开关sql："+sql);
+		String ZIDuan = "VALUE";
+		DBUtils dbutil =new DBUtils();
+		String KaiGuan = dbutil.queryString(sql,ZIDuan);
+		System.out.println("配置开关的值为："+KaiGuan);
+		if(KaiGuan.equals("0")){
+			System.out.println("关闭所有ping方法！全部返回false");
 			return false;
+		}else{
+			System.out.println("所有ping方法正常运行中！");
+			int counts = 0;
+			while (counts < 1) {
+			try {
+				URL url = new URL(urlStr);
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	    		con.setUseCaches(false);
+	    		con.setReadTimeout(1500);
+	    		con.setConnectTimeout(1500);
+				int state = con.getResponseCode();
+				System.out.println(counts +"= "+state);
+				if (state == 200) {
+					System.out.println("URL可用！");
+					return true;
+				}
+				break;
+			}catch (Exception ex) {
+				counts++;
+				System.out.println("URL不可用，连接第"+counts+"次");
+				urlStr = null;
+				continue;
+				}
+			}
+				return false;
+			}
 		}
+
 }
 
 
