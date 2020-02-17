9eb761f9a5f8ba66b7ca90b056c97ee736d2aa17
付有翔 <50285277+fuyouxiang@users.noreply.github.com>
2020-02-17 12:37:15 +0800

    增加提交申请的日志接口

--- src/cn/com/shxt/servlet/AddDanYuanTestServlet.java
+++ src/cn/com/shxt/servlet/AddDanYuanTestServlet.java
@@ -60,6 +60,13 @@ public class AddDanYuanTestServlet extends HttpServlet {
 
 		System.out.println(time+"添加单元测试申请："+sql);	
 
+		
+		String sql2 = "insert into SYS_TESTSQ_LOG (D_ID,T_PEOPLE,T_TIME,T_CAOZUO,T_BEIZHU) values ((select d_id from SYS_TEST_SQ  where D_DATE='" + date + "'),'" + kaifa + "','" + date + "','提交申请', '无')";
+
+		
+		int flag2 = dbutil.update(sql2);
+		System.out.println(time+"添加日志："+sql2);
+		
 		/*
 		try {
 			SendEmail sendEmail = new SendEmail();
